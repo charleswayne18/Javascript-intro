6666dcce9d73c8223b4ca3c93e99ceda0b7d7175
charleswayne18 <charleswayne18@gmail.com>
2023-12-07 22:42:40 +0100

    update

---
+++ boolean function/index.js
@@ -0,0 +1,58 @@
+function evenOrOdd(number,boolean) {
+    if (boolean === true) {
+        return addOddNumber(number)
+    }
+    else if (boolean === false) {
+        return addEvenNumber(number)
+    }
+}
+
+console.log(evenOrOdd (8, false))
+
+
+
+
+
+
+
+
+
+
+function addOddNumber(number) {
+    if (number < 0) {
+        return 0;
+    }    
+
+    let sum = 0;
+    let cycle = number;
+
+    while (cycle > 0) {
+        if (cycle % 2 !== 0) {
+            sum = sum + cycle;
+        }
+        
+        cycle = cycle - 1;
+    }      
+    return sum;
+}    
+
+function addEvenNumber(number) {
+    if (number < 0) {
+        return 0;
+    }    
+
+    let sum = 0;
+    let cycle = number;
+
+    while (cycle > 0) {
+        if (cycle % 2 == 0) {
+            sum = sum + cycle;
+        }
+        
+        cycle = cycle - 1;
+    }      
+    return sum;
+}    
+
+
+
